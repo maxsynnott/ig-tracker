3f1e4701f53d89e870dba007d04ea6ded6c0ea8f
maxsynnott <maxryansynnott@gmail.com>
2023-09-10 13:35:43 +0300

    Hello World API

---
+++ src/index.ts
@@ -0,0 +1,19 @@
+import fastify from "fastify";
+
+const server = fastify({
+	logger: true,
+});
+
+server.get("/", async (req, res) => {
+	return { message: "Hello World!" };
+});
+
+const start = async () => {
+	try {
+		await server.listen({ port: 3000 });
+	} catch (error) {
+		server.log.error(error);
+		process.exit(1);
+	}
+};
+start();
